dfc3d7cef6b1326932c02227d66ab96bbd06fa71
nidal197 <38545136+nidal197@users.noreply.github.com>
2018-04-23 15:21:41 +0300

    Update script1.py

--- 8port/script1.py
+++ 8port/script1.py
@@ -6,7 +6,7 @@ GPIO.setmode(GPIO.BCM)
 
 # init list with pin numbers
 
-pinList = [2, 3, 4, 17, 27, 22, 10, 9]
+pinList = [17, 27, 22, 5, 6, 13, 19, 26]
 
 # loop through pins and set mode and state to 'low'
 
@@ -21,28 +21,28 @@ SleepTimeL = 2
 # main loop
 
 try:
-  GPIO.output(2, GPIO.LOW)
+  GPIO.output(17, GPIO.LOW)
   print "ONE"
   time.sleep(SleepTimeL); 
-  GPIO.output(3, GPIO.LOW)
+  GPIO.output(27, GPIO.LOW)
   print "TWO"
   time.sleep(SleepTimeL);  
-  GPIO.output(4, GPIO.LOW)
+  GPIO.output(22, GPIO.LOW)
   print "THREE"
   time.sleep(SleepTimeL);
-  GPIO.output(17, GPIO.LOW)
+  GPIO.output(5, GPIO.LOW)
   print "FOUR"
   time.sleep(SleepTimeL);
-  GPIO.output(27, GPIO.LOW)
+  GPIO.output(6, GPIO.LOW)
   print "FIVE"
   time.sleep(SleepTimeL);
-  GPIO.output(22, GPIO.LOW)
+  GPIO.output(13, GPIO.LOW)
   print "SIX"
   time.sleep(SleepTimeL);
-  GPIO.output(10, GPIO.LOW)
+  GPIO.output(19, GPIO.LOW)
   print "SEVEN"
   time.sleep(SleepTimeL);
-  GPIO.output(9, GPIO.LOW)
+  GPIO.output(26, GPIO.LOW)
   print "EIGHT"
   time.sleep(SleepTimeL);
   GPIO.cleanup()
